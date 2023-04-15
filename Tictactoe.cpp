06cd88577feddaba0c5b3aa070ffa815acdd8f18
Szymon Kwaśny <kwasny.szymon1@gmail.com>
2023-04-15 16:19:29 +0200

    Poprawa sprawdzenia wyników gry

--- Tictactoe.cpp
+++ Tictactoe.cpp
@@ -23,6 +23,7 @@ bool Tictactoe::CheckResult() {
 	if ((board[0] == 'X' && board[1] == 'X' && board[2] == 'X') ||
 		(board[3] == 'X' && board[4] == 'X' && board[5] == 'X') ||
 		(board[6] == 'X' && board[7] == 'X' && board[8] == 'X') ||
+		(board[0] == 'X' && board[3] == 'X' && board[6] == 'X') ||
 		(board[1] == 'X' && board[4] == 'X' && board[7] == 'X') ||
 		(board[2] == 'X' && board[5] == 'X' && board[8] == 'X') ||
 		(board[0] == 'X' && board[4] == 'X' && board[8] == 'X') ||
@@ -34,6 +35,7 @@ bool Tictactoe::CheckResult() {
 		((board[0] == 'O' && board[1] == 'O' && board[2] == 'O') ||
 			(board[3] == 'O' && board[4] == 'O' && board[5] == 'O') ||
 			(board[6] == 'O' && board[7] == 'O' && board[8] == 'O') ||
+			(board[0] == 'X' && board[3] == 'X' && board[6] == 'X') ||
 			(board[1] == 'O' && board[4] == 'O' && board[7] == 'O') ||
 			(board[2] == 'O' && board[5] == 'O' && board[8] == 'O') ||
 			(board[0] == 'O' && board[4] == 'O' && board[8] == 'O') ||
